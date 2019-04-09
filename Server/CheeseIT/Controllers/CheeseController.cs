1bbcb729b7352002e82a9d62fef08b89f3fec052
Augusto Varela Banchero <gustivarela@hotmail.com>
2019-04-09 19:49:14 -0300

    Initial commit for WebAPI

---
+++ Server/CheeseIT/Controllers/CheeseController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseIT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheeseController : ControllerBase
+    {
+        // GET: api/Cheese
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "queso1", "queso2" };
+        }
+
+        // GET: api/Cheese/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Cheese
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Cheese/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
